d61e0ff4b90255557aad49914b206bba404bef86
Valentín Veintemilla <valentinveintemilla@gmail.com>
2025-10-14 02:47:30 -0300

    Add extra configuration files to assure good practices in the project

--- src/Web.BackOffice/Pages/Privacy.cshtml.cs
+++ src/Web.BackOffice/Pages/Privacy.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Web.BackOffice.Pages;
@@ -16,4 +16,3 @@ public class PrivacyModel : PageModel
     {
     }
 }
-
