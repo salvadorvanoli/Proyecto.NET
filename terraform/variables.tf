f09247f437b98c3568971a01f39788c999ddb9f7
salvadorvanoli <141060855+salvadorvanoli@users.noreply.github.com>
2025-11-15 11:47:51 -0300

    Agregado el jwt token default para que se buildee correctamente

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -104,6 +104,7 @@ variable "jwt_secret" {
   description = "Secret key para firmar JWT tokens (mínimo 32 caracteres)"
   type        = string
   sensitive   = true
+  default = "ThisIsASecret"
 }
 
 variable "jwt_issuer" {
